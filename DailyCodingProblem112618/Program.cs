a6bff718278e99ac720af6a0f32229002f47265d
w2 <william.washington@live.com>
2018-11-26 20:47:59 -0500

    //this is close, but I am not multiplying enough. I believe I'm going to have to have a few complex loops to complete this.

--- DailyCodingProblem112618/Program.cs
+++ DailyCodingProblem112618/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace DailyCodingProblem112618
 {
@@ -8,12 +10,26 @@ namespace DailyCodingProblem112618
         {
             /*
                 This problem was asked by Uber.
-                Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
-                For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+                Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers 
+                in the original array except the one at i. For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. 
+                If our input was [3, 2, 1], the expected output would be [2, 3, 6].
                 Follow-up: what if you can't use division?
             */
 
+            int[] values1 = new int[] { 1, 2, 3, 4, 5 };
+            int[] i = new int[] { 1 };
 
+            var result = values1.Except(i);  
+            
+            foreach (var item in result)
+            {
+                int[] returnValues = new int[] { item * item };
+                foreach (int part in returnValues)
+                {                    
+                    Debug.WriteLine(part);
+                }                
+            }
+            //this is close, but I am not multiplying enough. I believe I'm going to have to have a few complex loops to complete this.
         }
     }
 }
